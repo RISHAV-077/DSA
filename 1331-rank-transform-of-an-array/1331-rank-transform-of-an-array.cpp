b8d1fc21611fea0d9f8ca9fbb2a300726a1a9ffc
RISHAV-077 <147075142+RISHAV-077@users.noreply.github.com>
2026-07-12 11:41:43 +0545

    Time: 39 ms (55.31%), Space: 43.9 MB (55.31%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+
+        int n = arr.size();
+
+        vector<int> temp = arr;
+
+        sort(temp.begin(), temp.end());
+
+        unordered_map<int,int> mpp;
+
+        int rank = 1;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(mpp.find(temp[i]) == mpp.end())
+            {
+                mpp[temp[i]] = rank;
+                rank++;
+            }
+        }
+
+        vector<int> ans(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            ans[i] = mpp[arr[i]];
+        }
+
+        return ans;
+    }
+};
