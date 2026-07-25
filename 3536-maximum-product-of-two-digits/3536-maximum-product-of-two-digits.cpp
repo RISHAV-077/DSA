e7ed8b49e766e05618283af3d91a3170b8a884a3
RISHAV-077 <147075142+RISHAV-077@users.noreply.github.com>
2026-07-25 14:29:24 +0530

    Time: 2 ms (19.27%), Space: 9 MB (42.3%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        vector<int>ans;
+        while(n>0){
+            int ni = n%10;
+            ans.push_back(ni);
+            n=n/10;
+        }
+        sort(ans.begin() , ans.end());
+        int sz = ans.size();
+        return ans[sz-1]*ans[sz-2];
+
+        
+    }
+};
