3e56fe940870233750ad717f4c5a0ad621287df3
RISHAV-077 <147075142+RISHAV-077@users.noreply.github.com>
2026-08-21 10:13:45 +0530

    Time: 34 ms (9.1%), Space: 82.2 MB (5.54%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int n, m, N;
+    vector<vector<vector<int>>> dp;
+    bool solve(int i, int j, int k, string& s1, string& s2, string& s3) {
+
+        if (k == N && i == n && j == m)
+            return true;
+
+        if (k == N)
+            return false;
+
+        if (dp[i][j][k] != -1)
+            return dp[i][j][k];
+
+        bool result = false;
+
+        if (i < n && s1[i] == s3[k])
+            result = result || solve(i + 1, j, k + 1, s1, s2, s3);
+
+        if (j < m && s2[j] == s3[k])
+            result = result || solve(i, j + 1, k + 1, s1, s2, s3);
+
+        return dp[i][j][k] = result;
+    }
+
+    bool isInterleave(string s1, string s2, string s3) {
+        n = s1.length();
+        m = s2.length();
+        N = s3.length();
+        if (n + m != N)
+            return false;
+        dp.resize(n + 1, vector<vector<int>>(m + 1, vector<int>(N + 1, -1)));
+        return solve(0, 0, 0, s1, s2, s3);
+    }
+};
