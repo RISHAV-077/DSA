6c35415e4d7586348614f56937637c2cc3111ee8
RISHAV-077 <147075142+RISHAV-077@users.noreply.github.com>
2026-09-09 08:48:10 +0530

    Time: 0 ms (100%), Space: 8.9 MB (84.8%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long start = 1000;
+        long long result=0;
+        while(start<= n){
+            result += (n- start +1);
+            start*=1000;
+
+        }
+        return result;
+    }
+};
