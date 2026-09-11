5caf5903819c8a1e7de88c88c3f8572f617f0b02
RISHAV-077 <147075142+RISHAV-077@users.noreply.github.com>
2026-09-11 16:38:18 +0530

    Time: 15 ms (17.48%), Space: 13.5 MB (89.13%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        string result = "";
+        int n = s.length();
+        
+// forward eleiminate
+        int lastOpen = 0;
+        for(int i = 0; i<n; i++) {
+            char c = s[i];
+            if((c >= 'a' && c <= 'z'))
+                result.push_back(c);
+            else if(c == '(') {
+                result.push_back(c);
+                lastOpen++;
+            } else if(lastOpen > 0) {
+                lastOpen--;
+                result.push_back(c);
+            }
+        }
+        
+        if(result == "")
+            return "";
+        
+        
+        s = result;
+        result = "";
+        int lastClose = 0;
+        n = s.length();
+// backward elminate
+        for(int i = n-1; i>=0; i--) {
+            char c = s[i];
+            if((c >= 'a' && c <= 'z'))
+                result.push_back(c);
+            else if(c == ')') {
+                result.push_back(c);
+                lastClose++;
+            } else if(lastClose > 0) {
+                lastClose--;
+                result.push_back(c);
+            }
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
